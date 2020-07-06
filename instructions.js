4baedcf84718e8ae490407ed3a8dd528b6d6291e
Devin Owen <devinjamesowen@gmail.com>
2020-07-06 10:26:54 -0400

    initial commit

---
+++ instructions.js
@@ -0,0 +1,16 @@
+// under assignments on canvas
+//static layout website
+
+
+
+//clone a website
+//dont worry about working on phone
+//options hulu, netflix, disney plus, elevenfifty.org, indiana.gov, 
+//just html and css
+
+
+//go into inspect tool to find certain images to download
+
+//at least height of window, than half another screen
+
+
